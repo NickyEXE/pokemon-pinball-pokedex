671f970b41fb729946ba15c419708a440e76b6f4
Nicky Dover <dovern42@gmail.com>
2024-03-24 01:10:11 -0400

    slightly more labeling

--- containers/LocationsContainer.js
+++ containers/LocationsContainer.js
@@ -2,7 +2,8 @@ class LocationsContainer {
     render(){
         const main = document.querySelector("main") 
         main.innerHTML = `
-        <center>This lists all Pokemon you haven't yet caught, by location. Click on a Pokemon to catch it. Caught pokemon will appear grayed out.</center>
+        <center>This lists all Pokemon you haven't yet caught, by location. Click on a Pokemon to catch it.</center>
+        <center>Freshly caught Pokemon will appear grayed out. Pokemon marked as caught in the Pokedex won't appear at all.</center>
         <div class='locations'>
             <center><h2 class="red">Red Table</h2></center>
             <div id="red" class="red"></div>
--- containers/PokedexInputContainer.js
+++ containers/PokedexInputContainer.js
@@ -2,6 +2,7 @@ class PokedexInputContainer {
     render = () => {
         const main = document.querySelector("main") 
         main.innerHTML = `<center>Click on a Pokemon to catch it. Caught pokemon will appear grayed out.</center>
+        <center>Saved Pokemon will stay saved on page refresh, assuming you don't clear local data.</center>
         <div class='flex pokedex'></div>`
         this.div = main.querySelector("div")
         PokemonCheckbox.all = []
